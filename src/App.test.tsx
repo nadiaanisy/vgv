aa1125782e66c0240ad1058337985f9478bae3e4
Isyqi Saiful <saif@sitecore.net>
2025-09-24 16:04:31 +0800

    initial

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import {
+  render,
+  screen
+} from '@testing-library/react';
 
 test('renders learn react link', () => {
   render(<App />);
