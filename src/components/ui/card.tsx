417088844232a5e5f3df27b88f34204a9c17e0fe
Isyqi Saiful <saif@sitecore.net>
2025-09-26 10:29:35 +0800

    fix bug of couldnt auto scroll

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,22 +1,26 @@
 import { cn } from '../misc';
 import * as React from 'react';
 
-function Card({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card"
-      className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
+const Card = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        data-slot="card"
+        className={cn(
+          "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border",
+          className,
+        )}
+        {...props}
+      />
+    );
+  }
+);
 
-function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
-  return (
+const CardHeader = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(
+  ({ className, ...props }, ref) => (
     <div
+      ref={ref}
       data-slot="card-header"
       className={cn(
         "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 pt-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
@@ -24,34 +28,37 @@ function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
       )}
       {...props}
     />
-  );
-}
+  )
+);
 
-function CardTitle({ className, children, ...props }: React.ComponentProps<"div">) {
-  return (
+const CardTitle = React.forwardRef<HTMLHeadingElement, React.ComponentProps<"h4">>(
+  ({ className, children, ...props }, ref) => (
     <h4
+      ref={ref}
       data-slot="card-title"
       className={cn("leading-none", className)}
       {...props}
     >
       {children ?? "Card Title"}
     </h4>
-  );
-}
+  )
+);
 
-function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
-  return (
+const CardDescription = React.forwardRef<HTMLParagraphElement, React.ComponentProps<"p">>(
+  ({ className, ...props }, ref) => (
     <p
+      ref={ref}
       data-slot="card-description"
       className={cn("text-muted-foreground", className)}
       {...props}
     />
-  );
-}
+  )
+);
 
-function CardAction({ className, ...props }: React.ComponentProps<"div">) {
-  return (
+const CardAction = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(
+  ({ className, ...props }, ref) => (
     <div
+      ref={ref}
       data-slot="card-action"
       className={cn(
         "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
@@ -59,28 +66,38 @@ function CardAction({ className, ...props }: React.ComponentProps<"div">) {
       )}
       {...props}
     />
-  );
-}
+  )
+);
 
-function CardContent({ className, ...props }: React.ComponentProps<"div">) {
-  return (
+const CardContent = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(
+  ({ className, ...props }, ref) => (
     <div
+      ref={ref}
       data-slot="card-content"
       className={cn("px-6 [&:last-child]:pb-6", className)}
       {...props}
     />
-  );
-}
+  )
+);
 
-function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
-  return (
+const CardFooter = React.forwardRef<HTMLDivElement, React.ComponentProps<"div">>(
+  ({ className, ...props }, ref) => (
     <div
+      ref={ref}
       data-slot="card-footer"
       className={cn("flex items-center px-6 pb-6 [.border-t]:pt-6", className)}
       {...props}
     />
-  );
-}
+  )
+);
+
+Card.displayName = "Card";
+CardHeader.displayName = "CardHeader";
+CardTitle.displayName = "CardTitle";
+CardDescription.displayName = "CardDescription";
+CardAction.displayName = "CardAction";
+CardContent.displayName = "CardContent";
+CardFooter.displayName = "CardFooter";
 
 export {
   Card,
